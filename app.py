d5b247c489eb2350cfbeadee59625d2d8f0ff247
magapochta12-star <magapochta12@gmail.com>
2026-05-25 07:28:58 +0500

    Update app.py
    добавил функицю отправки фото с ограничениям до 5 мб, хранится будет 20 фото

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.config['SECRET_KEY'] = 'change-me-to-something-random'
 socketio = SocketIO(app, async_mode='threading', cors_allowed_origins='*')
 
 voice_messages = []
+image_messages = []   # храним последние 20 фото
 
 @app.route('/')
 def index():
@@ -15,8 +16,11 @@ def index():
 @socketio.on('connect')
 def handle_connect():
     emit('user_joined', {'msg': 'Кто-то присоединился'}, broadcast=True)
+    # Отправляем новому клиенту историю
     for vm in voice_messages[-10:]:
         emit('voice_message', vm)
+    for im in image_messages[-10:]:
+        emit('image_message', im)
 
 @socketio.on('disconnect')
 def handle_disconnect():
@@ -36,10 +40,21 @@ def handle_voice(data):
         'audio': data['audio']
     }
     voice_messages.append(msg)
-    # Оставляем только последние 50 голосовых, чтобы не переполнять память
     if len(voice_messages) > 50:
         voice_messages.pop(0)
     emit('voice_message', msg, broadcast=True)
+
+@socketio.on('image_message')
+def handle_image(data):
+    msg = {
+        'username': data.get('username', 'Аноним'),
+        'image': data['image'],   # base64
+        'mime': data.get('mime', 'image/jpeg')
+    }
+    image_messages.append(msg)
+    if len(image_messages) > 20:
+        image_messages.pop(0)
+    emit('image_message', msg, broadcast=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
