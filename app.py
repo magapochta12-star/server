8f7b068e297ef96b118622a5bbfa97713fbfa048
magapochta12-star <magapochta12@gmail.com>
2026-05-25 16:29:51 +0500

    Update app.py
    убрал звонки, так как не работают все способы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import os
 
@@ -6,13 +6,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'change-me-to-something-random'
 socketio = SocketIO(app, async_mode='threading', cors_allowed_origins='*')
 
-# Хранилища сообщений
 voice_messages = []
 image_messages = []
-file_messages = []
-
-# Активные пользователи: sid -> username
-connected_users = {}
+file_messages = []    # храним последние 10 файлов
 
 @app.route('/')
 def index():
@@ -21,7 +17,6 @@ def index():
 @socketio.on('connect')
 def handle_connect():
     emit('user_joined', {'msg': 'Кто-то присоединился'}, broadcast=True)
-    # Отправляем историю новичку
     for vm in voice_messages[-10:]:
         emit('voice_message', vm)
     for im in image_messages[-10:]:
@@ -29,18 +24,9 @@ def handle_connect():
     for fm in file_messages[-10:]:
         emit('file_message', fm)
 
-@socketio.on('register')
-def handle_register(data):
-    username = data.get('username', 'Аноним')
-    connected_users[request.sid] = username
-    emit('update_user_list', list(connected_users.values()), broadcast=True)
-
 @socketio.on('disconnect')
 def handle_disconnect():
-    if request.sid in connected_users:
-        del connected_users[request.sid]
     emit('user_left', {'msg': 'Кто-то вышел'}, broadcast=True)
-    emit('update_user_list', list(connected_users.values()), broadcast=True)
 
 @socketio.on('text_message')
 def handle_text(data):
@@ -51,7 +37,10 @@ def handle_text(data):
 
 @socketio.on('voice_message')
 def handle_voice(data):
-    msg = {'username': data.get('username', 'Аноним'), 'audio': data['audio']}
+    msg = {
+        'username': data.get('username', 'Аноним'),
+        'audio': data['audio']
+    }
     voice_messages.append(msg)
     if len(voice_messages) > 50:
         voice_messages.pop(0)
@@ -74,7 +63,7 @@ def handle_file(data):
     msg = {
         'username': data.get('username', 'Аноним'),
         'filename': data['filename'],
-        'file_data': data['file_data'],
+        'file_data': data['file_data'],   # base64
         'mime': data.get('mime', 'application/octet-stream'),
         'size': data.get('size', 0)
     }
@@ -82,64 +71,6 @@ def handle_file(data):
     if len(file_messages) > 10:
         file_messages.pop(0)
     emit('file_message', msg, broadcast=True)
-
-# ------------------ Звонки (WebRTC сигналинг) ------------------
-@socketio.on('call_user')
-def handle_call(data):
-    """Переслать предложение конкретному пользователю"""
-    target_sid = None
-    for sid, name in connected_users.items():
-        if name == data['to_username']:
-            target_sid = sid
-            break
-    if not target_sid:
-        emit('call_error', {'message': 'Пользователь не найден'})
-        return
-    emit('incoming_call', {
-        'from_username': data['from_username'],
-        'signal': data['signal']
-    }, room=target_sid)
-
-@socketio.on('call_accepted')
-def handle_call_accepted(data):
-    """Ответ вызывающему, что звонок принят"""
-    target_sid = None
-    for sid, name in connected_users.items():
-        if name == data['to_username']:
-            target_sid = sid
-            break
-    if target_sid:
-        emit('call_accepted', {'signal': data['signal']}, room=target_sid)
-
-@socketio.on('call_rejected')
-def handle_call_rejected(data):
-    target_sid = None
-    for sid, name in connected_users.items():
-        if name == data['to_username']:
-            target_sid = sid
-            break
-    if target_sid:
-        emit('call_rejected', {'message': 'Звонок отклонён'}, room=target_sid)
-
-@socketio.on('hang_up')
-def handle_hang_up(data):
-    target_sid = None
-    for sid, name in connected_users.items():
-        if name == data['to_username']:
-            target_sid = sid
-            break
-    if target_sid:
-        emit('call_ended', {}, room=target_sid)
-
-@socketio.on('ice_candidate')
-def handle_ice(data):
-    target_sid = None
-    for sid, name in connected_users.items():
-        if name == data['to_username']:
-            target_sid = sid
-            break
-    if target_sid:
-        emit('ice_candidate', {'candidate': data['candidate']}, room=target_sid)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
